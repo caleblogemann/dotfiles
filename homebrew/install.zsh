2cfaefa9f341931033e64c120d8686e2868b02fa
Caleb Logemann <logemann@iastate.edu>
2017-06-05 07:38:23 -0500

    Updated dotfiles

--- homebrew/install.zsh
+++ homebrew/install.zsh
@@ -1,13 +1,16 @@
 # Check to make sure xcode and xcode command line tools are installed
-xcode-select -p
-if [[ "$?" != "0" ]]; then
-    xcode-select --install
-fi
+#xcode-select -p
+#if [[ "$?" != "0" ]]; then
+#    xcode-select --install
+#fi
 
 # install homebrew if not already installed
 if [[ ! -x /usr/local/bin/brew ]]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+brew update
+brew upgrade
 
 brew tap Hombrew/bundle
 brew bundle
